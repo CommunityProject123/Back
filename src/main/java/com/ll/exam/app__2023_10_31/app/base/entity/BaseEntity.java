0c97a05f80b039e35a71ebb5aeb29688573c86c2
jinsu291 <wlstn291@naver.com>
2023-10-31 14:36:01 +0900

    Test case

---
+++ src/main/java/com/ll/exam/app__2023_10_31/app/base/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.ll.exam.app__2023_10_31.app.base.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+    @Transient // 아래 필드가 DB 필드가 되는 것을 막는다.
+    @Builder.Default
+    private Map<String, Object> extra = new LinkedHashMap<>();
+
+    public BaseEntity(long id) {
+        this.id = id;
+    }
+}
