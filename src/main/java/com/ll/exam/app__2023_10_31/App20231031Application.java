a2c22e61197a6db1bb4f0ba5db9da65ae721494d
jinsu291 <wlstn291@naver.com>
2023-10-31 14:02:37 +0900

    setting

---
+++ src/main/java/com/ll/exam/app__2023_10_31/App20231031Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2023_10_31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20231031Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App20231031Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/app__2023_10_31/App20231031ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2023_10_31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20231031ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
