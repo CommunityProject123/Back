ff9e81544c1c8bc68520053e2f591a93e1f7baf0
jinsu291 <wlstn291@naver.com>
2023-11-08 10:04:51 +0900

    add corsConfigurationSource

--- src/main/java/com/ll/exam/app__2023_10_31/app/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/app__2023_10_31/app/security/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -27,8 +30,8 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated() // 최소자격 : 로그인
                 )
-                .cors().disable() // 타 도메인에서 API 호출 가능
-                .csrf().disable() // CSRF 토큰 끄기
+                .cors();
+        http.csrf().disable() // CSRF 토큰 끄기
                 .httpBasic().disable() // httpBaic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
@@ -40,4 +43,47 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 특정 도메인만 허용
+        configuration.addAllowedOrigin("http://localhost:3000");
+
+//        configuration.addAllowedOrigin("*"); // 모든 도메인에서 요청 허용 (보안을 강화하려면 변경 필요)
+        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        configuration.addAllowedHeader("*"); // 모든 HTTP 헤더 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+
+        return source;
+    }
 }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        http
+//                .authorizeRequests(
+//                        authorizeRequests -> authorizeRequests
+//                                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+//                                .antMatchers("/member/login", "/member/join")
+//                                .permitAll()
+//                                .anyRequest()
+//                                .authenticated() // 최소자격 : 로그인
+//                )
+//                .cors().disable() // 타 도메인에서 API 호출 가능
+//                .csrf().disable() // CSRF 토큰 끄기
+//                .httpBasic().disable() // httpBaic 로그인 방식 끄기
+//                .formLogin().disable() // 폼 로그인 방식 끄기
+//                .sessionManagement(sessionManagement ->
+//                        sessionManagement.sessionCreationPolicy(STATELESS)
+//                ).addFilterBefore(
+//                        jwtAuthorizationFilter,
+//                        UsernamePasswordAuthenticationFilter.class
+//                );
+//
+//        return http.build();
+//    }
+//}
